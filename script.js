d5dbc9813c898fa05ba2297e8ed557040c4c6460
vinothkumarsakthivel <e21cs006@shanmugha.edu.in>
2023-03-19 12:47:47 +0530

    changing js file

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
-let todo = document.getElementById("todoinput").value;
+//Select form
+var form = document.getElementById("form");
+//getting input form inputfield
+var input = document.getElementById("input");
+//array to store the values
+let list = [];
+//submit
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
 
+    add();
+    //console.log('submit')
+})
+
+//function to add
+function add() {
+    let inputValue = input.value;
+
+    //Checking the input is empty or not empty
+    if (inputValue.length == 0) {
+        alert("Enter the text to add into list");
+    }
+    else {
+        //to store the value
+        let store = {
+            value: inputValue,
+            checked: false
+        }
+
+        list.push(store);
+
+        console.log(list);
+    }
+
+
+
+}
